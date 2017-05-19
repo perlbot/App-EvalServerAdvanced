f14e5244fdce6334bbfaa59e69cbedaa4549e1d2
Ryan Voots <ryan@voots.org>
2017-05-19 18:01:16 -0400

    Magic config system working

--- lib/EvalServer.pm
+++ lib/EvalServer.pm
@@ -3,6 +3,7 @@ package EvalServer;
 use strict;
 use EvalServer::Sandbox;
 use IO::Async::Loop;
+use IO::Async::Function;
 use EvalServer::Config;
 
 use Data::Dumper;
@@ -11,10 +12,23 @@ use Moo;
 
 has loop => (is => 'ro', lazy => 1, default => sub {IO::Async::Loop->new()});
 
+sub init {
+  my ($self) = @_;
+
+  my $func = IO::Async::Function->new(
+    max_workers => config('evalserver')->{max_workers},
+    max_worker_calls => config('evalserver')->{max_evals_per_worker} // 0,
+    idle_timeout => config('evalserver')->{worker_idle_timeout},
+    code => sub {
+      my ($evaldata) = @_; 
+
+  });
+}
+
 sub run {
   my ($self) = @_;
 
-  print Dumper(EvalServer::Config::config());
+  print Dumper(config()->language());
 }
 
 1;
--- lib/EvalServer/Config.pm
+++ lib/EvalServer/Config.pm
@@ -1,10 +1,17 @@
 package EvalServer::Config;
 
+use v5.20.0;
+
 use strict;
 use warnings;
 use TOML;
 use FindBin;
 use File::Slurper qw/read_text/;
+use Data::Dumper;
+
+require Exporter;
+our @ISA = qw/Exporter/;
+our @EXPORT = qw/config/;
 
 our $config;
 
@@ -13,15 +20,50 @@ sub load_config {
 
   my $data = read_text($file, "utf-8", "auto");
 
-  $config = TOML::from_toml($data);
+  my $makemagic = sub {
+    my $value = shift;
+
+    if (ref $value eq 'HASH') {
+      my $nv = +{map {; $_ => __SUB__->($value->{$_})} keys %$value};
+      return bless $nv, "EvalServer::Config::_magichash";
+    } elsif (ref $value eq 'ARRAY') {
+      return [map {__SUB__->($_)} @$value];
+    } else {
+      return $value;
+    }
+  };
+  
+  $config = $makemagic->(TOML::from_toml($data));
 }
 
 sub config {
+  my ($section) = @_;
   if (!defined $config) {
     load_config();
   };
 
   return $config;
+}
+
+package
+  EvalServer::Config::_magichash;
+use Carp qw/croak/;
+
+sub DESTROY {}
+
+our $AUTOLOAD;
+sub AUTOLOAD {
+  my ($self) = @_;
+  my $pack = __PACKAGE__;
+  my $meth = $AUTOLOAD;
+  $meth =~ s/^${pack}:://;
+
+  if (exists $self->{$meth}) {
+      return $self->{$meth};
+  } else {
+    croak "Config key [$meth] not found" if ($ENV{DEBUG});
+    return undef;
+  }
 }
 
 1;
