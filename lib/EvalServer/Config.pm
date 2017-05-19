35d3cf17d57dcaa620bf8bb2d6f3a4b480401a20
Ryan Voots <ryan@voots.org>
2017-05-19 17:05:28 -0400

    startup

---
+++ lib/EvalServer/Config.pm
@@ -0,0 +1,25 @@
+package EvalServer::Config;
+
+use strict;
+use warnings;
+use TOML;
+use FindBin;
+use File::Slurper;
+
+our $config;
+
+sub load_config {
+  my $file = $FindBin::Bin."/../etc/config.toml";
+
+  my $data = read_text($file, "utf-8", "auto");
+
+  $config = TOML::from_toml($data);
+}
+
+sub config {
+  if (!defined $config) {
+    load_config();
+  };
+
+  return $config;
+}
