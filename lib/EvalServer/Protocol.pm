c4dcd7724dc7b4a97657aaa4e85a41c2fe98ad69
Ryan Voots <simcop2387@simcop2387.info>
2017-05-22 14:41:48 -0700

    Protocol buffer support in place.  Seems to work.

--- lib/EvalServer/Protocol.pm
+++ lib/EvalServer/Protocol.pm
@@ -1,7 +1,25 @@
 package EvalServer::Protocol;
 
-use Google::ProtocolBuffers;
-Google::ProtocolBuffers->parse("protocol.protobuf",
-    {create_accessors => 1 }
-);
- 
+use strict;
+use warnings;
+use Data::Dumper;
+use FindBin;
+use EvalServer::Log;
+use Google::ProtocolBuffers::Dynamic;
+use Path::Tiny qw/path/;
+# TODO this should be more reliable
+my $path = path($FindBin::Bin . "/../lib/EvalServer/protocol.proto");
+debug $path->realpath;
+
+# load_file tries to allocate >100TB of ram.  Not sure why.
+open(my $fh, "<", $path->realpath);
+my $proto = do {local $/; <$fh>};
+close($fh);
+
+my $gpb = Google::ProtocolBuffers::Dynamic->new();
+
+$gpb->load_string("protocol.proto", $proto);
+
+$gpb->map({ pb_prefix => "messages", prefix => "EvalServer::Protocol", options => {accessor_style => 'single_accessor'} });
+
+1;
